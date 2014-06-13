fd17b75a8123d81daea57b37890c18b6835ba3de
Julian <julian@j42.me>
2014-06-13 11:51:40 -0400

    Class name fix

--- src/j42/LaravelFirebase/LaravelFirebaseFacade.php
+++ src/j42/LaravelFirebase/LaravelFirebaseFacade.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Facade;
 
-class Firebase extends Facade {
+class LaravelFirebaseFacade extends Facade {
 
     protected static function getFacadeAccessor() { return 'firebase'; }
 
