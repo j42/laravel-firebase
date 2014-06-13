e09f444460de2f921436cf136cf7c1b6821de726
Julian <julian@j42.me>
2014-06-13 04:52:39 -0400

    Initial commit :)

---
+++ src/J42/LaravelFirebase/LaravelFirebaseServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace J42\LaravelFirebase;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelFirebaseServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('j42/laravel-firebase', 'firebase');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
